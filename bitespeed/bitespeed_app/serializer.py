3284f7df63ebf6d45786899a9ffaa6187c6c4628
himansh-7004 <83622354+himansh-7004@users.noreply.github.com>
2024-06-20 02:36:21 +0530

    Delete bitespeed directory

--- bitespeed/bitespeed_app/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from .models import contact  
-
-class ContactSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = contact
-        fields = '__all__'
--- bitespeed/bitespeed_app/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-
-from bitespeed_app import views
-
-
-urlpatterns = [
-   path('', views.post_identity, name='post_data')
-]
